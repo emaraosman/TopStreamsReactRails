94476b600f9dd8e502c9fba4d5ae1236c8d4cee8
Osman Emara <emaraosman@gmail.com>
2018-03-20 16:35:19 -0400

    wrote mock model methods to insert api data in notes.js

--- notes.js
+++ notes.js
@@ -6,3 +6,9 @@ response = RestClient::Request.execute(method: :get, url: 'https://api.twitch.tv
 
 //test create for TopStreams_development:
 TwitchStream.create(streamer_id: '51496027', channel_name: 'loltyler1', viewers: '22892', followers: '1524858', total_views: '58567491', game: 'League of Legends', url: 'https://www.twitch.tv/loltyler1', logo: 'https://static-cdn.jtvnw.net/jtv_user_pictures/f3591dbe4ee3d94b-profile_image-300x300.png', likes: 0)
+
+//test a find_or_initialize_by then update based on existence method:
+TwitchStream.find_or_initialize_by(:streamer_id => 51496027).update_attributes!(:viewers => 22892)
+
+//Test of checking and not updating certain fields:
+TwitchStream.find_or_initialize_by(:streamer_id => 51496027, :channel_name => "loltyler1", :url => "https://www.twitch.tv/loltyler1").update_attributes!(:viewers => 22892, :followers=>100, :total_views => 100, :game => "PUBG", :logo => "newlogo", :likes => 10)
